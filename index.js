c8321a5d14c92dbec71e80660a002d253f67f019
Minh Tran <tdaimike@gmail.com>
2023-03-12 12:39:02 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+const newHeader= document.createElement("H1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
